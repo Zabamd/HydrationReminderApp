2de755e77c3fdb4d79beba035c95a6de0b1de072
Amadeusz Zabierowski <zabierowskiamadeusz@gmail.com>
2022-06-11 21:56:22 +0200

    Add project files.

---
+++ HydrationReminderApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HydrationReminderApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace HydrationReminderApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ HydrationReminderApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using HydrationReminderApp.Models;
+using HydrationReminderApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HydrationReminderApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
